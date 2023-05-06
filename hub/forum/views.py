446c123b061274dcf49d193be16626208d18d3fa
yokerdwork <yokerd.work@gmail.com>
2023-05-06 14:23:32 +0300

    test5

--- hub/forum/views.py
+++ hub/forum/views.py
@@ -1,15 +1,18 @@
 from django.http import HttpResponse, HttpResponseNotFound
 from django.shortcuts import render
+from .models import *
 
 # Create your views here.
+menu = ["Главная страница", "Обновления", "О системе", "Обратная связь", "Войти"]
 def index(request):
-    return render(request, 'forum/index.html')
+    return render(request, 'forum/index.html', {'menu': menu, 'title': 'Главная страница'})
 
 def about(request):
-    return render(request, 'forum/about.html')
+    return render(request, 'forum/about.html', {'menu': menu, 'title': 'О форуме'})
 
 def forum(request, forumid):
     return HttpResponse(f'<h1>Страница форум</h1><p>{forumid}</p>')
 
 def pageNotFound(request, exception):
     return HttpResponseNotFound(f'<h1>Страница не найдена: 404</h1>')
+
